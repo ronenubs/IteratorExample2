04e1e8a9b92d3405d3af452c555482ea5917d767
Ron Enubs <ronenubs@gmail.com>
2020-11-04 11:10:12 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(int number in EvenSequence(5, 18))
+            {
+                Console.Write(number.ToString() + " ");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static System.Collections.Generic.IEnumerable<int>
+            EvenSequence(int firstNumber, int lastNumber)
+        {
+            for(int number = firstNumber; number <= lastNumber; number++)
+            {
+                if(number % 2 == 0)
+                {
+                    yield return number;
+                }
+            }
+        }
+    }
+}
